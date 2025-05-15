32adbe39c11a671a608e92181e0fd7704c39b820
mrk1311 <mpruszkowski11@gmail.com>
2025-05-15 22:48:20 +0200

    styling favourite events

--- app/userPage/favourites.tsx
+++ app/userPage/favourites.tsx
@@ -6,19 +6,22 @@ import {
     TouchableOpacity,
     FlatList,
     ActivityIndicator,
+    Image,
 } from "react-native";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { supabase } from "@/utils/supabase";
 import { useUser } from "@/hooks/useUser";
 import { EventFeature } from "@/components/ClusteredMap";
+import { AntDesign } from "@expo/vector-icons";
+import getMarkerColor from "@/functions/getMarkerColor";
 
 // Define proper response type for the joined data
 type FavoriteResponse = {
     event_id: {
         id: string;
-        name: string;
+        title: string;
         type: string;
         description: string;
         link: string;
@@ -65,7 +68,7 @@ export default function Favourites() {
                     geometry: item.event_id.geometry,
                     properties: {
                         id: item.event_id.id,
-                        name: item.event_id.name,
+                        name: item.event_id.title,
                         type: item.event_id.type,
                         description: item.event_id.description,
                         link: item.event_id.link,
@@ -92,12 +95,78 @@ export default function Favourites() {
         }
     }, [user?.id]);
 
-    const renderItem = ({ item }: { item: EventFeature }) => (
-        <View style={styles.eventCard}>
-            <Text style={styles.eventTitle}>{item.properties.name}</Text>
-            <Text>{item.properties.description}</Text>
-            <Text>{new Date(item.properties.date).toLocaleDateString()}</Text>
-        </View>
+    const renderEventCard = useCallback(
+        ({ item }: { item: EventFeature }) => (
+            <TouchableOpacity
+                style={styles.cardContainer}
+                // onPress={() => {
+                //     props.openEventDetailsBottomSheet(item);
+                //     props.setCenter(
+                //         coordinatesToRegion(item.geometry.coordinates)
+                //     );
+                //     props.snapToIndex(0);
+                // }}
+            >
+                {/* Event Image */}
+                {item.properties.photo && (
+                    <Image
+                        source={{ uri: item.properties.photo }}
+                        style={styles.cardImagePlaceholder}
+                        resizeMode="cover"
+                    />
+                )}
+
+                {/* Event Type Indicator */}
+                <View
+                    style={[
+                        styles.typeIndicator,
+                        {
+                            backgroundColor: getMarkerColor(
+                                item.properties.type
+                            ),
+                        },
+                    ]}
+                />
+                {/* Main Content */}
+                <View style={styles.cardContent}>
+                    <View style={styles.cardHeader}>
+                        <Text style={styles.cardTitle}>
+                            {item.properties.name}
+                        </Text>
+                        <TouchableOpacity
+                            onPress={() =>
+                                // TODO
+                                console.log(
+                                    "adding " +
+                                        item.properties.name +
+                                        " to local storage"
+                                )
+                            }
+                        >
+                            <AntDesign name="hearto" size={24} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.metaContainer}>
+                        <Text style={styles.cardSubtitle}>
+                            {item.properties.type.toUpperCase()}
+                        </Text>
+                        <Text style={styles.cardDate}>
+                            {new Date(
+                                item.properties.date
+                            ).toLocaleDateString()}
+                        </Text>
+                    </View>
+                    <Text
+                        style={styles.cardDescription}
+                        numberOfLines={2}
+                        ellipsizeMode="tail"
+                    >
+                        {item.properties.description}
+                    </Text>
+                </View>
+            </TouchableOpacity>
+        ),
+        []
     );
 
     return (
@@ -117,7 +186,7 @@ export default function Favourites() {
             ) : (
                 <FlatList
                     data={favorites}
-                    renderItem={renderItem}
+                    renderItem={renderEventCard}
                     keyExtractor={(item) => item.properties.id}
                     contentContainerStyle={styles.listContent}
                 />
@@ -152,20 +221,75 @@ const styles = StyleSheet.create({
     listContent: {
         padding: 16,
     },
-    eventCard: {
-        backgroundColor: "#fff",
-        borderRadius: 8,
-        padding: 16,
-        marginBottom: 12,
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 4,
-        elevation: 2,
-    },
+    // cardContainer: {
+    //     backgroundColor: "#fff",
+    //     borderRadius: 8,
+    //     padding: 16,
+    //     marginBottom: 12,
+    //     shadowColor: "#000",
+    //     shadowOffset: { width: 0, height: 2 },
+    //     shadowOpacity: 0.1,
+    //     shadowRadius: 4,
+    //     elevation: 2,
+    // },
     eventTitle: {
         fontSize: 18,
         fontWeight: "bold",
         marginBottom: 8,
+    },
+    cardImagePlaceholder: {
+        width: 80,
+        height: 80,
+        borderRadius: 8,
+        marginLeft: 8,
+        marginRight: 8,
+        backgroundColor: "#eeeeee",
+    },
+    cardContainer: {
+        flexDirection: "row",
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: "#eeeeee",
+    },
+    cardHeader: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    cardContent: {
+        flex: 1,
+        zIndex: 5000,
+    },
+    cardTitle: {
+        fontSize: 16,
+        fontWeight: "600",
+        color: "#333333",
+    },
+    metaContainer: {
+        flexDirection: "row",
+        marginTop: 4,
+    },
+    cardSubtitle: {
+        fontSize: 14,
+        color: "#666666",
+    },
+    cardDate: {
+        fontSize: 12,
+        color: "#999999",
+        marginLeft: 8,
+    },
+    cardDescription: {
+        fontSize: 14,
+        color: "#666666",
+        marginTop: 8,
+    },
+    cardImage: {
+        width: 80,
+        height: 80,
+        borderRadius: 8,
+        marginLeft: 16,
+    },
+    typeIndicator: {
+        width: 6,
+        marginRight: 12,
     },
 });
